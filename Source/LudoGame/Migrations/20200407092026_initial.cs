69675fd33e10286f25f66a27fcb595aad95d4d51
simbasang <sebastian.englund@live.se>
2020-04-07 11:22:11 +0200

    @simbasang first migration
    Co-Authored-By: Eric Sandberg <kaptenen@users.noreply.github.com>
    Co-Authored-By: Benjamin Ytterström <benytt@users.noreply.github.com>

---
+++ Source/LudoGame/Migrations/20200407092026_initial.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LudoGame.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Piece",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsActive = table.Column<bool>(nullable: false),
+                    HasFinished = table.Column<bool>(nullable: false),
+                    Steps = table.Column<int>(nullable: false),
+                    Position = table.Column<int>(nullable: false),
+                    Color = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Piece", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Player",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    HasFinished = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Player", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GameState",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NextPlayerID = table.Column<int>(nullable: false),
+                    HasFinished = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameState", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_GameState_Player_NextPlayerID",
+                        column: x => x.NextPlayerID,
+                        principalTable: "Player",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameState_NextPlayerID",
+                table: "GameState",
+                column: "NextPlayerID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GameState");
+
+            migrationBuilder.DropTable(
+                name: "Piece");
+
+            migrationBuilder.DropTable(
+                name: "Player");
+        }
+    }
+}
